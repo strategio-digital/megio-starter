477968792856a19c9a891e322986df90d6f78fa7
Jiří Zapletal <jz@strategio.dev>
2023-10-08 13:36:53 -0600

    Updated megio-panel

--- assets/panel.ts
+++ assets/panel.ts
@@ -2,8 +2,7 @@
 import 'megio-panel/styles'
 import { createMegioPanel } from 'megio-panel'
 
-const apiUrl = window.location.host.includes('localhost') ? 'http://localhost:8090/' : '/'
-createMegioPanel(apiUrl)
+createMegioPanel(window.location.host.includes('localhost') ? 'http://localhost:8090/' : '/')
 
 // Advanced setup
 // https://github.com/strategio-digital/megio-panel
