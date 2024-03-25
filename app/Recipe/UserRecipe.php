6622e79095b3505df313bf4157209bf19d14b1b8
Jiří Zapletal <jz@strategio.dev>
2024-03-24 19:00:24 -0600

    Added Recipe Serializers

--- app/Recipe/UserRecipe.php
+++ app/Recipe/UserRecipe.php
@@ -17,6 +17,7 @@ use Megio\Collection\WriteBuilder\Rule\UniqueRule;
 use Megio\Collection\CollectionRecipe;
 use Megio\Collection\WriteBuilder\Field\EmailField;
 use Megio\Collection\WriteBuilder\Field\PasswordField;
+use Megio\Collection\WriteBuilder\Serializer\CallableSerializer;
 use Megio\Collection\WriteBuilder\WriteBuilder;
 use Megio\Collection\WriteBuilder\Rule\EqualRule;
 use Megio\Collection\WriteBuilder\Rule\MinRule;
@@ -61,18 +62,22 @@ class UserRecipe extends CollectionRecipe
         $items = array_map(fn($role) => new SelectField\Item($role->getId(), $role->getName()), $roles);
         
         return $builder
-            //->ignoreRules(['email' => [UniqueRule::class]])
+            ->ignoreRules(['email' => [CallableRule::class]])
             ->add(new EmailField(name: 'email', label: 'E-mail', rules: [
                 new RequiredRule(),
                 new UniqueRule(entityClassName: User::class, columnName: 'email', message: 'Tento e-mail je již použit.'),
                 new CallableRule(fn($value) => $value === 'jz@strategio.dev', 'E-mail není jz@strategio.dev.'),
+            ], serializers: [
+                new CallableSerializer(function($value) { return $value; } )
             ]))
+            
             ->add(new PasswordField(name: 'password', label: 'Heslo', rules: [
                 new RequiredRule(),
                 new MinRule(6),
                 new MaxRule(32)
             ]))
             ->add(new PasswordField(name: 'password_check', label: 'Heslo znovu', rules: [
+                new RequiredRule(),
                 new EqualRule('password'),
             ], mapToEntity: false))
             ->add(new SelectField(name: 'role', label: 'Role', items: $items, rules: [
