6972269b00981949a298c73fbaafb93c129bad40
Jiří Zapletal <jz@strategio.dev>
2024-04-24 19:32:43 -0600

    Updated to latest dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     resolve: {
@@ -8,6 +9,7 @@ export default defineConfig({
         }
     },
     plugins: [
+        vue(),
         laravel({
             publicDirectory: 'www',
             buildDirectory: 'temp',
