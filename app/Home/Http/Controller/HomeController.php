4a0b6e5a6b545051710796a42d44c05e2481e7d8
Jiří Zapletal <jz@strategio.dev>
2025-12-02 05:22:45 +0100

    Add locale-based redirection to HomeController

--- app/Home/Http/Controller/HomeController.php
+++ app/Home/Http/Controller/HomeController.php
@@ -5,12 +5,21 @@ namespace App\Home\Http\Controller;
 
 use Megio\Helper\Path;
 use Megio\Http\Controller\Base\Controller;
+use Megio\Translation\Translator;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller
 {
-    public function index(): Response
+    public function __construct(
+        private readonly Translator $translator,
+    ) {}
+
+    public function index(?string $locale = null): Response
     {
+        if ($locale === $this->translator->getDefaultShortCodeFromEnv()) {
+            return $this->redirect('home');
+        }
+
         return $this->render(Path::viewDir() . '/home/controller/home.latte');
     }
 }
