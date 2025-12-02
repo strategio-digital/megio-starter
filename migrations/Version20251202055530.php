e70a6ce2598a4fa29058d6c7a575d073edb65d3c
Jiří Zapletal <jz@strategio.dev>
2025-12-02 07:11:29 +0100

    Update migration to Version20251202055530

--- migrations/Version20251202055530.php
+++ migrations/Version20251202055530.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251107150634 extends AbstractMigration
+final class Version20251202055530 extends AbstractMigration
 {
     public function getDescription(): string
     {
